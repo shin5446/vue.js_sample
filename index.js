7a097ccbe753a06038638f6d2e90231a6b31a957
shin <5446shin@gmail.com>
2019-03-16 15:45:09 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    name: 'A山B郎',
+    cource: 'Webエンジニアコース',
+    acceptance_period: '2019年01月期',
+    students: [
+      { id: 1, name: '野呂浩良', cource: '機械学習コース', acceptance_period: '2019年01月期' },
+      { id: 2, name: '富永修司', cource: 'Webエンジニアコース', acceptance_period: '2017年11月期' },
+      { id: 3, name: '斉藤一起', cource: 'Webエンジニアコース', acceptance_period: '2017年11月期' }
+    ]
+  },
+  methods: {
+    addStudent: function() {
+        app.students.push({ id: app.students.length + 1, name: app.name, cource: app.cource, acceptance_period: app.acceptance_period  })
+      }
+    }
+})
